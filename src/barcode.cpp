0ccba9ab405eaf6349249172364349bad1646b56
rzryr1 <solotogre@gmail.com>
2025-01-31 22:55:41 +0700

    barcode new

--- src/barcode.cpp
+++ src/barcode.cpp
@@ -34,6 +34,46 @@ string convertToBarcodeFormat(const string& barcode) {
     return formattedBarcode;  
 }  
 
+string generateLuckyReward(int length) {  
+    string luckyNumber = "";  
+    for (int i = 0; i < length; i++) {  
+        luckyNumber += to_string(rand() % 10);  
+    }  
+    return luckyNumber;  
+}  
+
+void checkLuckyReward(const string& barcode) {  
+    string lastTwoDigits = barcode.substr(barcode.length() - 2);  
+    string lastThreeDigits = barcode.substr(barcode.length() - 3);  
+
+    string luckyTwo = generateLuckyReward(2);  
+    string luckyThree = generateLuckyReward(3);  
+    string luckyFull = generateLuckyReward(12);  
+
+    bool hasReward = false;  
+
+    cout << "\nCheck Your Lucky Rewards:\n";  
+
+    if (lastTwoDigits == luckyTwo) {  
+        cout << "Congratulations! You won with the last 2 digits: " << luckyTwo << "!\n";  
+        hasReward = true;  
+    }  
+
+    if (lastThreeDigits == luckyThree) {  
+        cout << "Congratulations! You won with the last 3 digits: " << luckyThree << "!\n";  
+        hasReward = true;  
+    }  
+
+    if (barcode == luckyFull) {  
+        cout << "Jackpot! You won with the full barcode: " << luckyFull << "!\n";  
+        hasReward = true;  
+    }  
+
+    if (!hasReward) {  
+        cout << "Sorry, no rewards this time.\n";  
+    }  
+}  
+
 int main() {  
     srand(time(0)); 
 
@@ -43,6 +83,6 @@ int main() {
 
     cout << formattedBarcode << endl;  
     cout << barcode << endl;  
-
+    checkLuckyReward(barcode);
     return 0;  
 }  
