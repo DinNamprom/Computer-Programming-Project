ee9341682a7c72ca49e17fb2bdf4490c743141fc
DinNamprom <56390732+DinNamprom@users.noreply.github.com>
2025-02-01 21:32:36 +0700

    Data Reader & Writer
    Read data from file.txt then write it in array to store data

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main() {
     cout << "loading data..." << endl;
-    cout << "loading data succesfull" << endl;
     int n=0;
-    countorder("data\\order.txt",n);
-    cout << n;
+    countorder("data\\products\\names.txt",n);
+    string name[n],code[n],price[n];
+    readorder("data\\products\\names.txt",n, name);
+    readorder("data\\products\\code.txt",n, code);
+    readorder("data\\products\\price.txt",n, price);
+    cout << "loading data succesfull" << endl;
+    for (int i=0;i<n;i++) {
+        cout << name[i] << ":" << code[i] << " " << price[i] << " baht" << endl;
+    }
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+for (int j=0;j<n;j++) {
+    //     cout << name[j] << ":" << code[j] << " " << price[j] << "฿" << endl;
+    // }
