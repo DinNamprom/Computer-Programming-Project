cd63508bf56bdbd4c76746d5f12cb5cc9ad9f1d4
pepsi9551 <0nuttawat0@gmail.com>
2025-02-27 11:52:15 +0700

    rework billgen#3 + fix barcode

--- src/billgen.cpp
+++ src/billgen.cpp
@@ -1,23 +1,39 @@
 #include <bits/stdc++.h>
 #include "calculate.cpp"
 #include "ascii_text_gen.cpp"
+#include "barcode.cpp"
 
 using namespace std;
 
 // string filePath = "C:\\Users\\mrcat\\Compro\\Computer-Programming-Project\\src\\bill.txt";
 string filePath = "..\\src\\test.txt";
 
-// vector<order> order = {
-//     {"Apple", 101, 3, 21.0},
-//     {"Banana", 102, 1, 12.0},
-//     {"Orange", 103, 2, 15.0},
-//     {"Grapes", 104, 4, 30.0},
-//     {"Pine", 105, 1, 25.0},
-//     {"Cat", 112, 1, 250.0}
-// };
-// vector<Item> items = convertOrdersToItems(order);
-// vector<double> z = calculateSummary(Itemprocessor(items));
-// vector<ItemResult> results = Itemprocessor(items);
+//จาก barcode
+int barcodeLength = 12;  
+string barcode = generateBarcodeNumber(barcodeLength);  
+string formattedBarcode = convertToBarcodeFormat(barcode); 
+
+void CreateBarcode(string formattedBarcode, string barcode, ofstream& bill) {
+    bill <<"|    " << formattedBarcode << "    |" << endl;
+    bill << "| " << setw(45) << barcode << setw(36) << " |" << endl;    
+    checkLuckyReward(barcode);
+    timenow();
+}
+
+
+//จาก calculate
+vector<order> order = {
+    {"Apple", 101, 3, 21.0},
+    {"Banana", 102, 1, 12.0},
+    {"Orange", 103, 2, 15.0},
+    {"Grapes", 104, 4, 30.0},
+    {"Pine", 105, 1, 25.0},
+    {"Cat", 112, 1, 250.0}
+};
+vector<Item> items = convertOrdersToItems(order);
+vector<double> z = calculateSummary(Itemprocessor(items));
+vector<ItemResult> results = Itemprocessor(items);
+
 
 void CreateTopborder(const string& text, ofstream& bill) {
     bill << " _________________________________________________________________________________\n";
@@ -27,33 +43,36 @@ void CreateTopborder(const string& text, ofstream& bill) {
     bill << "| _______________________________________________________________________________ |\n";
 }
 
-void CreateItemList(ofstream& bill, const vector<ItemResult>& results, double totalAmount) {
+void CreateMiddle(ofstream& bill, const vector<ItemResult>& results, const vector<double>& summary) {
     bill << "|                                                                                 |\n";
     bill << "| Item Name        | Quantity      | Price/Unit    | Discount    | Total Price    |\n";
     bill << "|---------------------------------------------------------------------------------|\n";
     
     for (const auto& item : results) {
         // Print item line
-        bill << "| " << left << setw(17) << item.name
+        bill << "| " << left << setw(17) << item.name 
              << "| " << setw(14) << item.quantity
              << "| " << setw(14) << item.value1
              << "| " << setw(11) << item.howdis << "%"
              << "| " << setw(15) << item.discount << "|\n";
     }
     bill << "|---------------------------------------------------------------------------------|\n";
-    bill << "| Total Amount"  << ":" << right << setw(66) << totalAmount << " |\n";
+    bill << "| Total Amount:" << right << setw(66) << summary[0] << " |\n";
+    bill << "| VAT 7% :" << right << setw(71) << summary[1] << " |\n";  
+    bill << "| Total Amount (Including VAT):" << right << setw(50) << summary[2] << " |\n";
 }
 
 void CreateBottomborder(ofstream& bill) {
     bill << "|                                                                                 |\n";
+    CreateBarcode(formattedBarcode, barcode, bill);
     bill << "| _______________________________________________________________________________ |\n";
-    bill << "|                           *** Thank You ***                                     |\n";
+    bill << "|                                *** Thank You ***                                |\n";
     bill << "|_________________________________________________________________________________|\n";
     bill.close();
     system(("code " + filePath).c_str());
 }
 
-void createbill(string path,double amount,vector<ItemResult> itemr,vector<double> z) {
+void createbill(string path,vector<ItemResult> item,vector<double> z) {
     ofstream bill(path);
     if (!bill.is_open()) {
         cerr << "Failed to open the file." << endl;
@@ -65,12 +84,14 @@ void createbill(string path,double amount,vector<ItemResult> itemr,vector<double
 
     string text = "The Bill";
     CreateTopborder(text, bill);
-    CreateItemList(bill, itemr, amount);
+    CreateMiddle(bill, item, z);
     CreateBottomborder(bill);
 }
 
-// int main() {
-//     double totalAmount = z[2];
-//     createbill(filePath,totalAmount,results);
-//     return 0;
-// }
+int main() {
+    srand(time(0)); 
+    // double totalAmount = z[2];
+    // double Vat = z[1];
+    createbill(filePath,results,z);
+    return 0;
+}
